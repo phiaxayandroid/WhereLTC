41ec013af27c17a4add39193dc1e8619ee47088f
masterUNG <phrombutr@gmail.com>
2016-12-13 16:56:15 +0700

    Finish 13dec

--- app/src/main/java/appewtc/masterung/whereltc/SignUpActivity.java
+++ app/src/main/java/appewtc/masterung/whereltc/SignUpActivity.java
@@ -2,6 +2,7 @@ package appewtc.masterung.whereltc;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -9,6 +10,7 @@ public class SignUpActivity extends AppCompatActivity {
 
     //Explicit
     private EditText nameEditText, userEditText, passwordEditText;
+    private String nameString, userString, passwordString;
 
 
     @Override
@@ -26,7 +28,20 @@ public class SignUpActivity extends AppCompatActivity {
 
     public void clickSignUpSign(View view) {
 
-    }
+        nameString = nameEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //Check Space
+        if (nameString.equals("") || userString.equals("") || passwordString.equals("")) {
+
+            //Have Space
+            Log.d("13decV1", "Have Space");
+
+        }
+
+
+    }   // clickSign
 
 
 }   // Main Class
