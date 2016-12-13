052391f02459392d58f8359c83dd185bb109d46a
masterUNG <phrombutr@gmail.com>
2016-12-13 16:38:41 +0700

    clickSing

--- app/src/main/java/appewtc/masterung/whereltc/SignUpActivity.java
+++ app/src/main/java/appewtc/masterung/whereltc/SignUpActivity.java
@@ -1,13 +1,32 @@
 package appewtc.masterung.whereltc;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText nameEditText, userEditText, passwordEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //Bind Widget
+        nameEditText = (EditText) findViewById(R.id.editText);
+        userEditText = (EditText) findViewById(R.id.editText2);
+        passwordEditText = (EditText) findViewById(R.id.editText3);
+
+
+    }   // Main Method
+
+    public void clickSignUpSign(View view) {
+
     }
-}
+
+
+}   // Main Class
