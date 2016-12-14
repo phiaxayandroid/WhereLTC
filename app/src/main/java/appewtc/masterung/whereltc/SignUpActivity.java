98c9f22c6d0fc5ec6944635d2a66b0cc73e6832c
masterUNG <phrombutr@gmail.com>
2016-12-14 14:40:54 +0700

    Near OK

--- app/src/main/java/appewtc/masterung/whereltc/SignUpActivity.java
+++ app/src/main/java/appewtc/masterung/whereltc/SignUpActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -51,6 +52,12 @@ public class SignUpActivity extends AppCompatActivity {
 
                 String s = updateUser.get();
                 Log.d("14decV1", "Result ==> " + s);
+
+                if (Boolean.parseBoolean(s)) {
+                    finish();
+                } else {
+                    Toast.makeText(SignUpActivity.this, "Cannot Update User", Toast.LENGTH_SHORT).show();
+                }
 
             } catch (Exception e) {
                 Log.d("14decV1", "e singUp ==> " + e.toString());
