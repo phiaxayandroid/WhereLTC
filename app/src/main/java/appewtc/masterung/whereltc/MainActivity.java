9447d0c11800dac20761f86d1663046faea9b23a
masterUNG <phrombutr@gmail.com>
2016-12-14 15:23:42 +0700

    Add Content

--- app/src/main/java/appewtc/masterung/whereltc/MainActivity.java
+++ app/src/main/java/appewtc/masterung/whereltc/MainActivity.java
@@ -4,14 +4,41 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        userEditText = (EditText) findViewById(R.id.editText4);
+        passwordEditText = (EditText) findViewById(R.id.editText5);
+
+
     }   // Main Method
+
+    public void clickSignIn(View view) {
+
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        if (userString.equals("") || passwordString.equals("")) {
+            //Have Space
+          //  MyAlert myAlert = new MyAlert(MainActivity.this, )
+
+        } else {
+            //No Space
+
+        }
+
+    }   // clickSignIn
 
     public void clickSignUpMain(View view) {
         startActivity(new Intent(MainActivity.this, SignUpActivity.class));
